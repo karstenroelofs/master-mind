5ae473e182db976acc5bfa99d77e3f97066377f4
Karsten Roelofs <karsten.roelofs@ziggo.nl>
2025-09-30 17:03:37 +0200

    Completed rewrite

--- tui.py
+++ tui.py
@@ -1,53 +1,53 @@
 from game import MasterMind
-from pegs import CodePeg, KeyPeg
-import random as r
 
 
 class MasterMindTui:
     game: MasterMind
-    secret_length: int
+    code_length: int
     colours: int
 
-    def __init__(self, secret_length=4, colours=6):
-        self.secret_length = secret_length
+    def __init__(self, code_length=4, colours=6):
+        self.code_length = code_length
         self.colours = colours
-        code = input("Code: ")
-        if code == "random":
-            code = tuple(r.choice(range(colours)) for _ in range(secret_length))
-        elif len(code) == secret_length:
-            code = tuple(int(x) for x in code)
-        else:
-            print("Invallid code")
-            return
-        self.game = MasterMind(code)
+        self.game = MasterMind(self.get_code(), code_length, colours)
+
+    def get_code(self) -> list[int]:
+        code = []
+        while not code:
+            input_str = input("Code: ")
+            if input_str in ["random", "r", ""]:
+                return []
+            elif len(input_str) == self.code_length and all(
+                int(c) in range(self.colours) for c in input_str
+            ):
+                return [int(c) for c in input_str]
+            else:
+                print(f"Invalid code {input_str}, please try again")
+        return []
 
     def start(self):
-        while True:
+        while not self.game.won:
             self.print_state()
             query_str = input("Query: ")
-            if len(query_str) == self.secret_length:
-                query = tuple(int(x) for x in query_str)
-                self.game.guess_int(query)
+            if len(query_str) == self.code_length and all(
+                int(c) in range(self.colours) for c in query_str
+            ):
+                query = [int(x) for x in query_str]
+                self.game.guess(query)
             else:
-                print("Invallid query")
+                print("Invalid query")
+        print("Game won!")
 
     def print_state(self):
-        if self.game.results == (
-            KeyPeg.BLACK,
-            KeyPeg.BLACK,
-            KeyPeg.BLACK,
-            KeyPeg.BLACK,
-        ):
-            print("Game won!")
-        else:
-            for qs, rs in zip(self.game.guesses, self.game.results):
-                print(
-                    " ".join(str(q.value) for q in qs)
-                    + " | "
-                    + " ".join(str(r.value) for r in rs)
-                )
+        for qs, rs in zip(self.game.guesses, self.game.results):
+            print(" ".join(str(q) for q in qs) + " | " + " ".join(str(r) for r in rs))
+
+
+def print_game_state(game: MasterMind):
+    for qs, rs in zip(game.guesses, game.results):
+        print(" ".join(str(q) for q in qs) + " | " + " ".join(str(r) for r in rs))
 
 
 if __name__ == "__main__":
-    g = MasterMindTui()
+    g = MasterMindTui(6, 10)
     g.start()
